8943ec62b0ae470808132bad215d76e9225d5ad2
Dennis Kalochristianakis <dionyshs97@hotmail.com>
2018-02-28 17:20:38 +0200

    merge punctuation and comments
    !WARNING comments currently crash the program

--- scanner.h
+++ scanner.h
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-typedef enum{KEYWORD,IDENTIFIER,INT_CONST,REAL_CONST,STRING,OPERATOR,PUNCTUATION} type;
+typedef enum{KEYWORD,IDENTIFIER,INT_CONST,REAL_CONST,STRING,OPERATOR,PUNCTUATION,COMMENT} type;
 
 typedef struct{
 	int number_of_token;
