c1a8bba376b3e7efdc107145219465be296beff6
Dennis Kalochristianakis <dionyshs97@hotmail.com>
2018-02-28 14:57:23 +0200

    Added numbers, formated output, created a token subtype for alpha_token_t

--- scanner.h
+++ scanner.h
@@ -6,11 +6,14 @@
 
 using namespace std;
 
+typedef enum{KEYWORD,IDENTIFIER,INT_CONST,REAL_CONST,STRING,OPERATOR} type;
+
 typedef struct{
 	int number_of_token;
 	int line_number;
-	string str_type; 
+	type token_type; 
+	string str_subType;
 	string token_value;
 }alpha_token_t;
 
-void addToken(string token_t, string value, int line);
+void addToken(type token_t,string token_sT, string value, int line);
