173ecbbc95cf9ba9540956d354bb60e661c1afa8
ERIONA NIKOLLI <erionanikolli1@gmail.com>
2018-02-28 23:47:26 +0200

    static array

--- string_coments.cpp
+++ string_coments.cpp
@@ -1,10 +1,7 @@
-
-  std::vector<int> com_v (10);   // 10 zero-initialized elements
-
   
-  std::vector<int>::size_type sz = com_v.size();
 "/*"						
-{ 	int sum=1;
+{ 	int com_v [100];
+	int sum=1;
 	int even=2;
 	com_v[0]=yylineno;
 	int odd=1;
@@ -41,11 +38,11 @@
 	else
 	{
 		int i=0;
-		int j=com_v.size();
+		int j=sizeof(com_v);
 		
-		while(i<com_v.size() && j>0)
+		while(i<sizeof(com_v) && j>0)
 		{
-			string s1="Multiline Comment from line ",com_v[i]," to line ",com_v[j],";
+			string s1="Multiline Comment from line ",com_v[i]," to line ",com_v[j]," ";
 			void addToken(type token_t,string token_sT, string value, int line);
 		}
 	}
