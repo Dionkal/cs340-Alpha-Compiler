b1f84d37b68dfaa188cb11d1733fcd5ae5b4a5b0
Yyna <csd3405@csd.uoc.gr>
2018-05-21 18:20:11 +0300

    2 previous files not usefullafter all. now ceating avm.h

--- createOutputFiles.cpp
+++ createOutputFiles.cpp
@@ -1,7 +1,6 @@
-#inlude <iostream>
-#inlude <fstream>
 
-avmbinaryfile:	magicnumber arrays code 					{/*TODO generate 2 files:binary and text*/
+
+/*TODO generate 2 files:binary and text*/
 																fstream outputBinary,outputText;
 
 																outputBinary.open("output.abc", ios::binary);
@@ -15,44 +14,3 @@ avmbinaryfile:	magicnumber arrays code 					{/*TODO generate 2 files:binary and 
 																	outputText<</*plain text code load our staff*/;
 																}
 																outputText.close();
-
-															}
-
-magicnumber: 	34020051 /*should be unsigned*/
-
-arrays:			strings numbers userfunctions libfunctions 	{}
-
-strings:		total (string)* /*oeo*/						{}
-
-total:			unsigned									{}
-
-string:			size (char)*/*null terminated*/				{}
-
-size:			unsigned									{}
-
-numbers:		total (double*)								{}
-
-userfunctions:	total (userfunc*)							{}
-
-userfunc: 		address localsize id						{}
-
-address:		unsigned									{}
-
-localsize:		unsigned									{}
-
-id:				string 										{}
-
-libfunctions:	strings										{}
-
-code:			total(instruction)*							{}
-
-instruction:	opcode operand operand operand				{}
-
-opcode:			byte 										{}
-
-operand: 		type value 									{}
-
-type:			byte 										{}
-
-value:			unsigned									{}
-
