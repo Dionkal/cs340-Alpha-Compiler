ad746ffd1f36a4e09cf5051b222d8c60a6377dff
Yyna <csd3405@csd.uoc.gr>
2018-05-27 01:01:58 +0300

    ex_math with 1 warning

--- VM/ex_math.cpp
+++ VM/ex_math.cpp
@@ -34,13 +34,13 @@ arithmetic_func_t arithmeticFuncs[]={
 };
 
 void execute_arithmetic(instruction *i){
-	arithmetic_func_t op;
+	//arithmetic_func_t op;
 
 	avm_memcell *lv = avm_translate_operand(&i->vm_result, (avm_memcell*) 0);
 	avm_memcell *rv1 = avm_translate_operand(&i->vm_arg1, &ax);
 	avm_memcell *rv2 = avm_translate_operand(&i->vm_arg2, &bx);
 
-	assert(lv && (&stack[AVM_STACKSIZE-1] && lv>&stack[top] || lv==&retval));
+	assert(((lv && (&stack[AVM_STACKSIZE-1] )&& lv>&stack[top]) || lv==&retval));
 
 	if(rv1->type != number_m ||rv2->type!=number_m){
 		//avm_error("Not a number in arithmetic");
