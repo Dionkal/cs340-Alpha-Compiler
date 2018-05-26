1841fe211ea751ea4788f0ed23aac6be9a3c98ee
Dennis Kalochristianakis <dionyshs97@hotmail.com>
2018-05-26 16:39:07 +0300

    WIP assignments

---
+++ VM/ex_assign.cpp
@@ -0,0 +1,30 @@
+#include "ex_assign.h"
+
+
+void avm_assign(avm_memsell* lv, avm_memsell* rv){
+	if(lv==rv) return;
+
+	if((lv->type == table_m) && (rv->type == table_m) && (lv->data.tableVar == rv->data.tableVar)) return;
+
+	if(rv->tpe == undef_m) //throw warning 
+		return;
+
+	avm_memsellclear(lv);
+
+	memcpy(lv, rv, sizeof(avm_memsell)); //dispath instead in cpp
+
+	if (lv->type == string_m)
+		lv->data.strVal = strdup(rv->data.strVal);
+	else if (lv->type == table_m)
+		avm_tableincrefcounter(lv->data.tableVar);
+
+}
+void execute_assign (instruction * instr){
+	avm_memsell* lv = avm_translate_operand(&instr->result, (avm_memsell*)0);
+	avm_memsell* rv = avm_translate_operand(&instr->arg1, &ax;
+
+	assert((lv && ((&stack[N-1] >= lv) && (lv > &stack[top]))) || lv==&retval);
+	assert(rv);
+
+	avm_assign(lv,rv); 
+}
