8790454e79db016d4c471bada44f2e9726fe8bbe
Yyna <csd3405@csd.uoc.gr>
2018-05-26 19:30:32 +0300

    debuging executes

--- VM/ex_assign.cpp
+++ VM/ex_assign.cpp
@@ -1,7 +1,7 @@
 #include "ex_assign.h"
 
 
-void avm_assign(avm_memsell* lv, avm_memsell* rv){
+void avm_assign(avm_memcell* lv, avm_memcell* rv){
 	if(lv==rv) return;
 
 	if((lv->type == table_m) && (rv->type == table_m) && (lv->data.tableVar == rv->data.tableVar)) return;
@@ -21,9 +21,9 @@ void avm_assign(avm_memsell* lv, avm_memsell* rv){
 }
 void execute_assign (instruction * instr){
 	avm_memsell* lv = avm_translate_operand(&instr->result, (avm_memsell*)0);
-	avm_memsell* rv = avm_translate_operand(&instr->arg1, &ax;
+	avm_memsell* rv = avm_translate_operand(&instr->arg1, &ax);
 
-	assert((lv && ((&stack[N-1] >= lv) && (lv > &stack[top]))) || lv==&retval);
+	assert((lv && ((&stack[AVM_STACKSIZE-1] >= lv) && (lv > &stack[top]))) || lv==&retval);
 	assert(rv);
 
 	avm_assign(lv,rv); 
--- VM/ex_assign.h
+++ VM/ex_assign.h
@@ -1,1 +1,8 @@
+#include "avm.h"
+#include "avm_table.h"
+#include <string.h>
+#include <stdlib.h>
+
+void avm_assign(avm_memcell* lv, avm_memcell* rv);
+
 void execute_assign (instruction *);
