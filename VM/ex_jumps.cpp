a2bd726e72e20d48781abcc5862b6d33aba80cb7
Dennis Kalochristianakis <dionyshs97@hotmail.com>
2019-05-24 21:05:54 +0300

    Implemented rest of branching executions

--- VM/ex_jumps.cpp
+++ VM/ex_jumps.cpp
@@ -1,4 +1,5 @@
 #include "ex_jumps.h"
+#include <string.h>
 
 extern avm_memcell ax, bx, cx, retval;
 extern avm_memcell stack[AVM_STACKSIZE];
@@ -70,6 +71,26 @@ void execute_jeq(instruction *i)
 		else
 		{
 			/*TODO::Equality check with dispatching (rv1)*/
+			switch (rv1->type)
+			{
+			case number_m:
+				result = rv1->data.numVal == rv2->data.numVal;
+				break;
+			case string_m:
+				result = !strcmp(rv1->data.strVal, rv2->data.strVal);
+				break;
+			case table_m:
+				result = rv1->data.tableVal == rv2->data.tableVal;
+				break;
+			case userfunc_m:
+				result = rv1->data.funcVal == rv2->data.funcVal;
+				break;
+			case libfunc_m:
+				result = rv1->data.libfuncVal == rv2->data.libfuncVal;
+				break;
+			default:
+				assert(0);
+			}
 		}
 	}
 
@@ -79,25 +100,280 @@ void execute_jeq(instruction *i)
 
 void execute_jne(instruction *i)
 {
-	/* TODO: implementation */
+	unsigned char result = 0;
+
+	assert(i->vm_result.type == label_a);
+
+	avm_memcell *rv1 = avm_translate_operand(&i->vm_arg1, &ax);
+	avm_memcell *rv2 = avm_translate_operand(&i->vm_arg2, &bx);
+
+	if (rv1->type == undef_m || rv2->type == undef_m)
+	{
+		avm_error("'undef' involved in non equal");
+	}
+	else
+	{
+		if (rv1->type == nil_m || rv2->type == nil_m)
+		{
+			result = (unsigned char)nil_m;
+		}
+		else if (rv1->type == bool_m || rv2->type == bool_m)
+		{
+			result = (unsigned char)bool_m;
+		}
+		else if (rv1->type != rv2->type)
+		{
+			avm_error("%s== %s is illegal", typeStrings[rv1->type], typeStrings[rv2->type]);
+		}
+		else
+		{
+			switch (rv1->type)
+			{
+			case number_m:
+				result = rv1->data.numVal != rv2->data.numVal;
+				break;
+			case string_m:
+				result = strcmp(rv1->data.strVal, rv2->data.strVal);
+				break;
+			case table_m:
+				result = rv1->data.tableVal != rv2->data.tableVal;
+				break;
+			case userfunc_m:
+				result = rv1->data.funcVal != rv2->data.funcVal;
+				break;
+			case libfunc_m:
+				result = rv1->data.libfuncVal != rv2->data.libfuncVal;
+				break;
+			default:
+				assert(0);
+			}
+		}
+	}
+
+	if (!executionFinished && result)
+		pc = i->vm_result.val;
 }
 
 void execute_jle(instruction *i)
 {
-	/* TODO: implementation */
+	unsigned char result = 0;
+
+	assert(i->vm_result.type == label_a);
+
+	avm_memcell *rv1 = avm_translate_operand(&i->vm_arg1, &ax);
+	avm_memcell *rv2 = avm_translate_operand(&i->vm_arg2, &bx);
+
+	if (rv1->type == undef_m || rv2->type == undef_m)
+	{
+		avm_error("'undef' involved in less equal");
+	}
+	else
+	{
+		if (rv1->type == nil_m || rv2->type == nil_m)
+		{
+			result = (unsigned char)nil_m;
+		}
+		else if (rv1->type == bool_m || rv2->type == bool_m)
+		{
+			result = (unsigned char)bool_m;
+		}
+		else if (rv1->type != rv2->type)
+		{
+			avm_error("%s== %s is illegal", typeStrings[rv1->type], typeStrings[rv2->type]);
+		}
+		else
+		{
+			switch (rv1->type)
+			{
+			case number_m:
+				result = rv1->data.numVal <= rv2->data.numVal;
+				break;
+			case string_m:
+				result = (strcmp(rv1->data.strVal, rv2->data.strVal) <= 0);
+				break;
+			case table_m:
+				result = rv1->data.tableVal <= rv2->data.tableVal;
+				break;
+			case userfunc_m:
+				result = rv1->data.funcVal <= rv2->data.funcVal;
+				break;
+			case libfunc_m:
+				result = rv1->data.libfuncVal <= rv2->data.libfuncVal;
+				break;
+			default:
+				assert(0);
+			}
+		}
+	}
+
+	if (!executionFinished && result)
+		pc = i->vm_result.val;
 }
 
 void execute_jge(instruction *i)
 {
-	/* TODO: implementation */
+	unsigned char result = 0;
+
+	assert(i->vm_result.type == label_a);
+
+	avm_memcell *rv1 = avm_translate_operand(&i->vm_arg1, &ax);
+	avm_memcell *rv2 = avm_translate_operand(&i->vm_arg2, &bx);
+
+	if (rv1->type == undef_m || rv2->type == undef_m)
+	{
+		avm_error("'undef' involved in greater equal");
+	}
+	else
+	{
+		if (rv1->type == nil_m || rv2->type == nil_m)
+		{
+			result = (unsigned char)nil_m;
+		}
+		else if (rv1->type == bool_m || rv2->type == bool_m)
+		{
+			result = (unsigned char)bool_m;
+		}
+		else if (rv1->type != rv2->type)
+		{
+			avm_error("%s== %s is illegal", typeStrings[rv1->type], typeStrings[rv2->type]);
+		}
+		else
+		{
+			switch (rv1->type)
+			{
+			case number_m:
+				result = rv1->data.numVal >= rv2->data.numVal;
+				break;
+			case string_m:
+				result = (strcmp(rv1->data.strVal, rv2->data.strVal) >= 0);
+				break;
+			case table_m:
+				result = rv1->data.tableVal >= rv2->data.tableVal;
+				break;
+			case userfunc_m:
+				result = rv1->data.funcVal >= rv2->data.funcVal;
+				break;
+			case libfunc_m:
+				result = rv1->data.libfuncVal >= rv2->data.libfuncVal;
+				break;
+			default:
+				assert(0);
+			}
+		}
+	}
+
+	if (!executionFinished && result)
+		pc = i->vm_result.val;
 }
 
 void execute_jlt(instruction *i)
 {
-	/* TODO: implementation */
+	unsigned char result = 0;
+
+	assert(i->vm_result.type == label_a);
+
+	avm_memcell *rv1 = avm_translate_operand(&i->vm_arg1, &ax);
+	avm_memcell *rv2 = avm_translate_operand(&i->vm_arg2, &bx);
+
+	if (rv1->type == undef_m || rv2->type == undef_m)
+	{
+		avm_error("'undef' involved in less than equal");
+	}
+	else
+	{
+		if (rv1->type == nil_m || rv2->type == nil_m)
+		{
+			result = (unsigned char)nil_m;
+		}
+		else if (rv1->type == bool_m || rv2->type == bool_m)
+		{
+			result = (unsigned char)bool_m;
+		}
+		else if (rv1->type != rv2->type)
+		{
+			avm_error("%s== %s is illegal", typeStrings[rv1->type], typeStrings[rv2->type]);
+		}
+		else
+		{
+			switch (rv1->type)
+			{
+			case number_m:
+				result = rv1->data.numVal < rv2->data.numVal;
+				break;
+			case string_m:
+				result = (strcmp(rv1->data.strVal, rv2->data.strVal) < 0);
+				break;
+			case table_m:
+				result = rv1->data.tableVal < rv2->data.tableVal;
+				break;
+			case userfunc_m:
+				result = rv1->data.funcVal < rv2->data.funcVal;
+				break;
+			case libfunc_m:
+				result = rv1->data.libfuncVal < rv2->data.libfuncVal;
+				break;
+			default:
+				assert(0);
+			}
+		}
+	}
+
+	if (!executionFinished && result)
+		pc = i->vm_result.val;
 }
 
 void execute_jgt(instruction *i)
 {
-	/* TODO: implementation */
+	unsigned char result = 0;
+
+	assert(i->vm_result.type == label_a);
+
+	avm_memcell *rv1 = avm_translate_operand(&i->vm_arg1, &ax);
+	avm_memcell *rv2 = avm_translate_operand(&i->vm_arg2, &bx);
+
+	if (rv1->type == undef_m || rv2->type == undef_m)
+	{
+		avm_error("'undef' involved in greater than equal");
+	}
+	else
+	{
+		if (rv1->type == nil_m || rv2->type == nil_m)
+		{
+			result = (unsigned char)nil_m;
+		}
+		else if (rv1->type == bool_m || rv2->type == bool_m)
+		{
+			result = (unsigned char)bool_m;
+		}
+		else if (rv1->type != rv2->type)
+		{
+			avm_error("%s== %s is illegal", typeStrings[rv1->type], typeStrings[rv2->type]);
+		}
+		else
+		{
+			switch (rv1->type)
+			{
+			case number_m:
+				result = rv1->data.numVal > rv2->data.numVal;
+				break;
+			case string_m:
+				result = (strcmp(rv1->data.strVal, rv2->data.strVal) > 0);
+				break;
+			case table_m:
+				result = rv1->data.tableVal > rv2->data.tableVal;
+				break;
+			case userfunc_m:
+				result = rv1->data.funcVal > rv2->data.funcVal;
+				break;
+			case libfunc_m:
+				result = rv1->data.libfuncVal > rv2->data.libfuncVal;
+				break;
+			default:
+				assert(0);
+			}
+		}
+	}
+
+	if (!executionFinished && result)
+		pc = i->vm_result.val;
 }
